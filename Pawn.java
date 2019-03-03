1d05f23b431c9c97292330a22549668de270ec51
decjgvsu <46975898+decjgvsu@users.noreply.github.com>
2019-03-03 01:10:44 -0500

    Piece movement restrictions added

--- Pawn.java
+++ Pawn.java
@@ -2,6 +2,8 @@ package p3;
 
 public class Pawn extends ChessPiece {
 
+    private boolean firstMove = true;
+
     public Pawn(Player player) {
         super(player);
     }
@@ -12,9 +14,86 @@ public class Pawn extends ChessPiece {
 
     // determines if the move is valid for a pawn piece
     public boolean isValidMove(Move move, IChessPiece[][] board) {
-
         boolean valid = true;
-        // More code is needed
+        int pathLength = move.fromRow - move.toRow;
+
+        // Prevents movement through other pieces vertically
+        if(move.fromRow != move.toRow){
+
+            // Checks path from high from row to low to row
+            if(move.fromRow > move.toRow){
+                for(int i = move.fromRow - 1; i > move.toRow; i--){
+                    if(board[i][move.fromColumn] != null){
+                        valid = false;
+                    }
+                }
+            }
+            // Checks path from low from row to high to row
+            if(move.fromRow < move.toRow){
+                for(int i = move.fromRow + 1; i < move.toRow; i++){
+                    if(board[i][move.fromColumn] != null){
+                        valid = false;
+                    }
+                }
+            }
+        }
+
+        // Cases where destination cell is populated
+        if (board[move.toRow][move.toColumn] != null) {
+
+            // Prevents capturing same color pieces
+            if (board[move.toRow][move.toColumn].player() == board[move.fromRow][move.fromColumn].player()) {
+                valid = false;
+            }
+            //Prevents capturing pieces directly ahead
+            if(move.toColumn == move.fromColumn){
+                valid = false;
+            }
+        }
+
+
+        // Prevents white pawns moving down
+        if(board[move.fromRow][move.fromColumn].player()
+                == Player.WHITE && pathLength < 0){
+            valid = false;
+        }
+
+        // Prevents white pawns moving up
+        if(board[move.fromRow][move.fromColumn].player()
+                == Player.BLACK && pathLength > 0){
+            valid = false;
+        }
+
+        // Absolute value of path Length
+        if(pathLength < 0){
+            pathLength = -pathLength;
+        }
+
+        // Prevents moving more than 2 spaces
+        if(pathLength > 2){
+            valid = false;
+        }
+        // Prevents moving 2 unless on first move
+        else if(pathLength == 2 && !firstMove){
+            valid = false;
+        }
+
+        // Prevents capturing itself
+        if ((move.fromRow == move.toRow) && (move.fromColumn == move.toColumn)) {
+            valid = false;
+        }
+
+        // Prevents horizontal movement
+        if(!(move.fromColumn == move.toColumn)){
+            if(board[move.toRow][move.toColumn] == null){
+                valid = false;
+            }
+        }
+
+        // Must be right before return, tracks first move
+        if(valid){
+            firstMove = false;
+        }
         return valid;
     }
 }
--- Rook.java
+++ Rook.java
@@ -19,6 +19,72 @@ public class Rook extends ChessPiece {
 
         boolean valid = true;
         // More code is needed
+
+        if (((move.fromRow == move.toRow) && (move.fromColumn == move.toColumn)) == false) {
+
+            // Prevents capturing same color pieces
+            if (board[move.toRow][move.toColumn] != null) {
+                if (board[move.toRow][move.toColumn].player() == board[move.fromRow][move.fromColumn].player()) {
+                    valid = false;
+                }
+            }
+
+
+//            System.out.println(board[move.toRow][move.toColumn].player());
+
+            // Prevents non-horizontal or non-vertical movement
+            if (!((move.fromRow == move.toRow) ^ (move.fromColumn == move.toColumn))) {
+                valid = false;
+            }
+
+            // Prevents movement through other pieces vertically
+            if (move.fromRow != move.toRow) {
+
+                // Checks path from high from row to low to row
+                if (move.fromRow > move.toRow) {
+                    for (int i = move.fromRow - 1; i > move.toRow; i--) {
+                        if (board[i][move.fromColumn] != null) {
+                            valid = false;
+                        }
+                    }
+                }
+                // Checks path from low from row to high to row
+                if (move.fromRow < move.toRow) {
+                    for (int i = move.fromRow + 1; i < move.toRow; i++) {
+                        if (board[i][move.fromColumn] != null) {
+                            valid = false;
+                        }
+                    }
+                }
+            }
+
+            // Prevents movement through other pieces horizontally
+            else if ((move.fromColumn != move.toColumn)) {
+
+                // Checks path from high from col to low to col
+                if (move.fromColumn > move.toColumn) {
+                    for (int i = move.fromColumn - 1; i > move.toColumn; i--) {
+                        if (board[move.toRow][i] != null) {
+                            valid = false;
+                        }
+                    }
+                }
+
+                // Checks path from low from col to high to col
+                else if (move.fromColumn < move.toColumn) {
+                    for (int i = move.fromColumn + 1; i < move.toColumn; i++) {
+                        if (board[move.toRow][i] != null) {
+                            valid = false;
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+
+
         return valid;
 
     }
